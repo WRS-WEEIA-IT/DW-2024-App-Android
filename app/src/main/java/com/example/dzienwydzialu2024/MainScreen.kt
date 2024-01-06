2bf901af61fbaf8e80c79fe3aef067badb8b3cb7
IgorJonski <jonskiigor@gmail.com>
2024-01-07 00:34:53 +0100

    Add qr code scanner button

--- app/src/main/java/com/example/dzienwydzialu2024/MainScreen.kt
+++ app/src/main/java/com/example/dzienwydzialu2024/MainScreen.kt
@@ -7,22 +7,20 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material.BottomNavigation
-import androidx.compose.material.BottomNavigationItem
-import androidx.compose.material.icons.Icons
-import androidx.compose.material3.BottomAppBar
-import androidx.compose.material3.Icon
-import androidx.compose.material3.IconButton
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.NavigationBar
-import androidx.compose.material3.Scaffold
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.BottomAppBar
+import androidx.compose.material.FabPosition
+import androidx.compose.material.FloatingActionButton
+import androidx.compose.material.Icon
+import androidx.compose.material.Scaffold
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
-import androidx.navigation.NavController
 import androidx.navigation.NavHostController
 import com.example.dzienwydzialu2024.navigation.BottomNavigationBar
 import com.example.dzienwydzialu2024.navigation.NavigationGraph
@@ -50,8 +48,28 @@ fun MainScreen(
             }
         },
         bottomBar = {
-            BottomNavigationBar(navController = navController)
-        }
+            BottomAppBar(
+                backgroundColor = DarkBlack,
+                elevation = 0.dp,
+            ) {
+                BottomNavigationBar(navController = navController)
+            }
+        },
+//        floatingActionButtonPosition = FabPosition.Center,
+//        isFloatingActionButtonDocked = true,
+//        floatingActionButton = {
+//            FloatingActionButton(
+//                shape = CircleShape,
+//                onClick = { /*TODO*/ },
+//                backgroundColor = Color.Magenta,
+//                contentColor = Color.White,
+//            ) {
+//                Icon(
+//                    painter = painterResource(id = R.drawable.ic_qr_code),
+//                    contentDescription = "QR Code Scanner"
+//                )
+//            }
+//        }
     ) {
         NavigationGraph(
             navController = navController,
--- app/src/main/java/com/example/dzienwydzialu2024/navigation/BottomNavItem.kt
+++ app/src/main/java/com/example/dzienwydzialu2024/navigation/BottomNavItem.kt
@@ -10,6 +10,7 @@ import com.example.dzienwydzialu2024.R
 sealed class BottomNavItem(val route: String, @DrawableRes val icon: Int, val title: String) {
     data object Home : BottomNavItem("home", R.drawable.ic_compass, "Home")
     data object Events : BottomNavItem("events", R.drawable.ic_study, "Events")
+    data object QrCodeScanner : BottomNavItem("qr_code_scanner", R.drawable.ic_qr_code, "QrCodeScanner")
     data object Tasks : BottomNavItem("tasks", R.drawable.ic_trophy, "Tasks")
     data object Info : BottomNavItem("info", R.drawable.ic_info, "Info")
 }
