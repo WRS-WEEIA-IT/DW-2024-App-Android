d9d248a9df76a9bd1fc9d8c13f79c374dbc1a50a
IgorJonski <jonskiigor@gmail.com>
2024-01-07 21:54:35 +0100

    Show correct tasks in HomeScreen

--- app/src/main/java/com/app/dw2024/home/HomeViewModel.kt
+++ app/src/main/java/com/app/dw2024/home/HomeViewModel.kt
@@ -9,6 +9,7 @@ import com.app.dw2024.R
 import com.app.dw2024.model.Event
 import com.app.dw2024.model.Task
 import com.app.dw2024.repository.interfaces.EventsRepository
+import com.app.dw2024.repository.interfaces.TasksRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.launch
 import java.time.LocalDateTime
@@ -16,27 +17,19 @@ import javax.inject.Inject
 
 @HiltViewModel
 class HomeViewModel @Inject constructor(
-    private val eventsRepository: EventsRepository
+    private val eventsRepository: EventsRepository,
+    private val tasksRepository: TasksRepository
 ): ViewModel() {
     var state by mutableStateOf(HomeState())
         private set
 
     init {
         viewModelScope.launch {
-            state = state.copy(tasks = listOf(
-                Task(1, "Zdjęcie zabytku", "Zrób zdjęcie znanego zabytku.", 10, false),
-                Task(2, "Kod szyfru", "Znajdź i rozszyfruj ukryty kod.", 15, false),
-                Task(3, "Quiz historyczny", "Odpowiedz na pytanie o historię miasta.", 5, false),
-                Task(4, "Skarb miejski", "Znajdź ukryty skarb w parku.", 20, false),
-                Task(5, "Ptaki parkowe", "Zidentyfikuj rodzaje ptaków w parku.", 10, false),
-                Task(6, "Pomnik selfie", "Zrób selfie z pomnikiem.", 5, false),
-                Task(7, "Śladami historii", "Śledź trasę historyczną miasta.", 15, false),
-                Task(8, "Zagadka architektoniczna", "Rozwiąż zagadkę dotyczącą budynku.", 10, false)
-            ))
+            val tasks = tasksRepository.getTasks()
             val events = eventsRepository.getEvents()
+            state = state.copy(tasks = tasks)
             state = state.copy(events = events)
         }
-
     }
 
     fun onEvent(event: HomeEvent) {
